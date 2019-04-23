a73b03cc549feb9ecdfc86afff4bdc5f4826a2cc
Avijeet <avijeet.cse1.bu@gmail.com>
2019-04-24 02:46:25 +0600

    Food Bloggers added

---
+++ Food_Bloggers/FBvirenv/Food_Bloggers/restaurantApp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Restaurant, Dish
+
+
+
+class RestaurantForm(forms.ModelForm):
+
+    class Meta:
+        model = Restaurant
+        exclude = ['user', 'date',]
+    
+
+
+class DishesForm(forms.ModelForm):
+
+    class Meta:
+        model = Dish
+        exclude = ['user', 'date', 'restaurant',]
---
+++ Food_Bloggers/FBvirenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/avijeet/Development/Food Bloggers/FBvirenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
